6013affc96ee00517bb1dc663fb00e35450755ad
keroppi <yejiseo0725@gmail.com>
2025-04-16 18:56:37 +0900

    visual_svg animation

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ $(function () {
   });
 });
 
+// header: scroll 반응에 따라 사라지게 하기
 $(function () {
   let prevScrollTop = 0;
 
@@ -23,3 +24,18 @@ $(function () {
     prevScrollTop = nowScrollTop;
   });
 });
+
+// svg.path 길이 구하기
+$(function () {
+  $(".svg-ani")
+    .find("#svg-ani01")
+    .each(function (i, path) {
+      let length = path.getTotalLength();
+      // alert(length);
+    });
+});
+
+// * each(): object, 배열 모두 사용할 수 있는 반복함수로
+// length 속성을 갖는 배열과 유사배열객체들의 index를 기준으로
+// 첫 번째 매개변수로 배열이나 객체를 받고,
+// 두 번째 매개변수로 콜백함수 값을 인자로 받음
